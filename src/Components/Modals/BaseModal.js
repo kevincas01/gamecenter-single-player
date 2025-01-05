072288e9b02c1fceea020600e2a5cc686ebc79aa
Kevin Castro <kcapupp@princeton.edu>
2025-01-04 19:59:51 -0500

    indent

--- src/Components/Modals/BaseModal.js
+++ src/Components/Modals/BaseModal.js
@@ -1,22 +1,19 @@
-import React from 'react'
-import '../../Styles/modals.css'
+import React from "react";
+import "../../Styles/modals.css";
 
-const BaseModal = ({title,close,children}) => {
+const BaseModal = ({ title, close, children }) => {
   return (
-    <div className='modal_container'>
-        
-        <div className='modal'>
-            
-
+    <div className="modal_container">
+      <div className="modal">
         <h1>{title}</h1>
-        
-            <div  onClick={close} className='close-modal'>X</div>
-        {children}
-        
+
+        <div onClick={close} className="close-modal">
+          X
         </div>
-
+        {children}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BaseModal
+export default BaseModal;
